ec2c2e535abac00651a2134fa9c1c1d98d3f2d11
Mirret0 <102880040+Mirret0@users.noreply.github.com>
2023-05-17 19:44:11 -0600

    updated models, first draft

---
+++ server/models/Clothes.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const clothesSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    itemUrl: {
+        type: String,
+        required: true,
+    validate: { 
+        validator: value => validator.isURL(value, { protocols: ['http','https','ftp'], require_tld: true, require_protocol: true }),
+        message: 'Must be a Valid URL' 
+        }
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    }
+});
+
+const Clothes = mongoose.model('Clothes', clothesSchema);
+
+module.exports = Clothes;
