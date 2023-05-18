4f26f8c2c5264470616b1a2367536660a73e31d6
Mirret0 <102880040+Mirret0@users.noreply.github.com>
2023-05-17 21:05:28 -0600

    typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type Query
-
-    type Mutation
-
     type User {
         _id: ID!
         username: String!
@@ -29,7 +25,26 @@ const typeDefs = gql`
         item5: Clothes
         item6: Clothes
         item7: Clothes
+      }   
+      
+      type Auth {
+        token: ID
+        user: User
       }
+
+      type Query {
+        user: User
+        outfit(_id: ID!): Outfit
+        outfits: [Outfit]
+        clothes(_id: ID!): Clothes 
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        addOutfit(item1: ID!, item2: ID!, item3: ID!, item4: ID, item5: ID, item1: ID, item6: ID, item1: ID, item7: ID, ): Outfit
+        addClothes(name: String!, itemURL: String!, imageURL: String!): Clothes
+        login(email: String!, password: String!): Auth
+    }
 `
 
 module.exports = typeDefs;
