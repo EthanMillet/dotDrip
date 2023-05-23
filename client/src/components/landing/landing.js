c77e540220cb8e536dc704f686c97efb34480313
Wodaloo <masonaviano@icloud.com>
2023-05-23 13:20:38 -0600

    fixedlanding.js

--- client/src/components/landing/landing.js
+++ client/src/components/landing/landing.js
@@ -4,49 +4,23 @@ import LandingCards from './landingCards';
 
 const Landing = () => {
     const [outfit, setOutfit] = useState({
-        name: '',
         clothes: [],
     });
-
-    const handleChange = (event) => {
-        const { name, value } = event.target;
-        setOutfit((prevOutfit) => ({
-            ...prevOutfit,
-            [name]: value,
-        }));
-    };
 
     const handleAddClothes = (clothes) => {
         setOutfit((prevOutfit) => ({
             ...prevOutfit,
-            clothes: [...prevOutfit.clothes, clothes],
+            clothes: [...prevOutfit.clothes, ...clothes],
         }));
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // Perform submit logic, such as saving the outfit to the database
-        console.log(outfit);
     };
 
     return (
         <div className="landing">
-            <div className="cards-div">
-                <h2>Create Outfit</h2>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="name">Outfit Name:</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={outfit.name}
-                        onChange={handleChange}
-                        required
-                    />
-                    <LandingCards onAddClothes={handleAddClothes} />
-                    <button type="submit">Save Outfit</button>
-                </form>
+            <div className="landing-header">
+                <h1>Look At All These Outfits</h1>
+                <Link to="/profile">Your Profile</Link>
             </div>
+            <LandingCards onAddClothes={handleAddClothes} />
         </div>
     );
 };
